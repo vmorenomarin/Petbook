36ad47a93b02ab45656709d2947bd0fb435af4db
vmorenomarin@gmail.com <vmorenomarin@gmail.com>
2016-08-22 18:29:51 -0500

    Generacion de adaptador, viewholders, y clases asociadas a las actividades

--- app/src/main/java/com/vmorenomarin/petbook/MainActivity.java
+++ app/src/main/java/com/vmorenomarin/petbook/MainActivity.java
@@ -35,12 +35,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initAdapter() {
+        PetAdapter adapter = new PetAdapter(pets);
+        petList.setAdapter(adapter);
     }
 
     private void initPetList() {
         pets = new ArrayList<Pet>();
 
         pets.add(new Pet(R.drawable.pet1, "Pelusa", 3, "Euclideo", 2345432));
-        pet.add(new Pet(R.drawable.pet2, "Gasparín",))
+        pets.add(new Pet(R.drawable.pet2, "Gasparín",5,"Lazaro",7845433));
+        pets.add(new Pet(R.drawable.pet3, "Misifú",2,"Isa",7845653));
+        pets.add(new Pet(R.drawable.pet4, "Elmo",2,"Julio",5683251));
+        pets.add(new Pet(R.drawable.pet5, "Anita",3,"Laura",873283));
+        pets.add(new Pet(R.drawable.pet6, "Salamita",1,"Gabriel",5837032));
+        pets.add(new Pet(R.drawable.pet7, "Lilith",1,"Diana",7845433));
+        pets.add(new Pet(R.drawable.pet8, "Hugin y Menin",5,"Elisa",3451237));
+        pets.add(new Pet(R.drawable.pet9, "Marti",1,"Alba",5649294));
+        pets.add(new Pet(R.drawable.pet10, "Zafir",1,"Alirio",4560219));
+
+
+
+
+
+
     }
 }
