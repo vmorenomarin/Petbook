3202cc5fb1c5c1d05fc68ec974cdfcf167cb6db3
vmorenomarin@gmail.com <vmorenomarin@gmail.com>
2016-08-22 14:51:08 -0500

    Creacion del POJO Pet.class

--- app/src/main/java/com/vmorenomarin/petbook/Pet.java
+++ app/src/main/java/com/vmorenomarin/petbook/Pet.java
@@ -4,4 +4,62 @@ package com.vmorenomarin.petbook;
  * Created by victor on 8/22/16.
  */
 public class Pet {
+    private int photo;
+    private String name;
+    private String master;
+    private int age;
+    private int phone;
+
+    public Pet(int photo, String name, String master, int age, int phone) {
+        this.photo = photo;
+        this.name = name;
+        this.master = master;
+        this.age = age;
+        this.phone = phone;
+
+
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaster() {
+        return master;
+    }
+
+    public void setMaster(String master) {
+        this.master = master;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
 }
+
+
+
